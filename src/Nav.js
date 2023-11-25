a633bf3670dc9f9df648d11ec2e3e6c6de2fb782
Adrian Booth <adrian.booth17@gmail.com>
2023-11-25 18:34:22 -0300

    Add countries visited to nav

--- src/Nav.js
+++ src/Nav.js
@@ -2,8 +2,11 @@ import * as React from 'react';
 
 export default function Nav () {
   return (
-    <header className="header">
-      <h1 className="logo"><a href="/">Main Gallery</a></h1>
-    </header>
+  <nav class="navbar">
+    <ul class="nav-links">
+      <li class="nav-item"><a href="/">Main Gallery</a></li>
+      <li class="nav-item"><a href="/map">Countries I've Visited</a></li>
+    </ul>
+  </nav>
   )
 }
