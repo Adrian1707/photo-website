c04799820b0551811e28433a9a40fce247ce05e7
Adrian Booth <adrian.booth17@gmail.com>
2023-11-26 19:41:37 -0300

    Add nav item back

--- src/Nav.js
+++ src/Nav.js
@@ -5,6 +5,7 @@ export default function Nav () {
   <nav className="navbar">
     <ul className="nav-links">
       <li className="nav-item"><a href="/">Main Gallery</a></li>
+      <li className="nav-item"><a href="/map">Countries I've visited</a></li>
     </ul>
   </nav>
   )
