2f84a7ab9b96c52d18439784ca3b03ceb52ef844
Adrian Booth <adrian.booth17@gmail.com>
2023-11-26 21:18:24 -0300

    Add new links to nav

--- src/Nav.js
+++ src/Nav.js
@@ -4,7 +4,11 @@ export default function Nav () {
   return (
   <nav className="navbar">
     <ul className="nav-links">
-      <li className="nav-item"><a href="/">Main Gallery</a></li>
+      <li className="nav-item"><a href="/">Countries</a></li>
+      <li className="nav-item"><a href="/wildlife">Wildlife</a></li>
+      <li className="nav-item"><a href="/candids">Candids</a></li>
+      <li className="nav-item"><a href="/architecture">Architecture</a></li>
+      <li className="nav-item"><a href="/landscape">Landscape</a></li>
       <li className="nav-item"><a href="/map">Countries I've visited</a></li>
     </ul>
   </nav>
