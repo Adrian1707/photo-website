9c0ae2c2515c37a3cb6db911655f4f5d7440eced
Adrian Booth <adrian.booth17@gmail.com>
2023-12-01 07:45:58 -0300

    Add flags to subnav

--- src/Nav.js
+++ src/Nav.js
@@ -9,7 +9,13 @@ export default function Nav () {
       <li className="nav-item"><a href="/candids">Candids</a></li>
       <li className="nav-item"><a href="/architecture">Architecture</a></li>
       <li className="nav-item"><a href="/landscape">Landscape</a></li>
-      <li className="nav-item"><a href="/map">Countries I've visited</a></li>
+      <li className="nav-item">
+        <a>Countries I've visited</a>
+        <ul className="dropdown">
+          <li><a href="/map">Map</a></li>
+          <li><a href="/flags">Flags</a></li>
+        </ul>
+      </li>
     </ul>
   </nav>
   )
