4ecaf37ef4fd0daa522c1b1df7cd33075c3e691b
Adrian Booth <adrian.booth17@gmail.com>
2023-12-19 10:30:04 -0300

    Update image url now CORs policy made more leniant on cloudfront CDN

--- src/ImageAPI.js
+++ src/ImageAPI.js
@@ -1,2 +1,2 @@
-// export const IMAGE_API = 'https://dq17sgdxquuwe.cloudfront.net'
-export const IMAGE_API = 'https://adrianboothphotos.s3.eu-west-2.amazonaws.com'
+export const IMAGE_API = 'https://dq17sgdxquuwe.cloudfront.net'
+// export const IMAGE_API = 'https://adrianboothphotos.s3.eu-west-2.amazonaws.com'
